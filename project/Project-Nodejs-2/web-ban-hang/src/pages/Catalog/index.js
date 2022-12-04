db603a2197264406b9da5f9284c14c43fea65f65
PhamTuan <chinblue23699@gmail.coom>
2022-12-04 16:05:02 +0700

    add

--- project/Project-Nodejs-2/web-ban-hang/src/pages/Catalog/index.js
+++ project/Project-Nodejs-2/web-ban-hang/src/pages/Catalog/index.js
@@ -1,11 +1,78 @@
-import { Empty } from "antd";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { skeletonProduct } from "../../assets/fake-data";
+import Grid from "../../Components/Grid";
 import Helmet from "../../Components/Helmet";
+import ProductCart from "../../Components/ProductCart";
+import { SectionBody } from "../../Components/Section";
+import SkeletonProducts from "../../Components/SkeletonProducts";
+import { setLoadingAction } from "../../Store/Reducer/loadingReducer";
+import {
+  productsSelector,
+  useGetAllProductsQuery,
+} from "../../Store/Reducer/productsReducer";
+// import { getProducts } from "../../utils";
 
 export default function Catalog() {
+  const { products } = useSelector(productsSelector);
+  const [pageNum, setPageNum] = useState(1);
+  const dispatch = useDispatch();
+
+  const { isLoading } = useGetAllProductsQuery({
+    pageNum: pageNum,
+    limitNum: 5,
+  });
+
+  const handleLoadmoreProduct = () => {
+    setPageNum((prevPageNum) => prevPageNum + 1);
+  };
+
+  useEffect(() => {
+    dispatch(setLoadingAction(isLoading));
+  }, [dispatch, isLoading, pageNum]);
+
   return (
     <Helmet title="Catalog">
-      <Empty />
+      <SectionBody>
+        <div className="cart-products">
+          <Grid col={5} mdCol={2} smCol={1} gap={0}>
+            {products?.map((item, index) => {
+              return (
+                <div key={index}>
+                  <ProductCart
+                    id={item._id}
+                    name={item.name}
+                    price={item.price}
+                    status={item.status}
+                    star={item.star}
+                    amount={item.amount}
+                    category={item.category}
+                    capacity={item.capacity}
+                    product={item}
+                    varation={item.varation}
+                    image={item.image}
+                    description={item.description}
+                    priceOld={item.priceOld}
+                    height="350"
+                    img_width="90%"
+                    right="5px"
+                    sold={item.sold}
+                  />
+                </div>
+              );
+            })}
+          </Grid>
+
+          <Grid col={5} mdCol={2} smCol={1} gap={0}>
+            {isLoading &&
+              skeletonProduct.map((item, index) => (
+                <SkeletonProducts key={index} />
+              ))}
+          </Grid>
+        </div>
+      </SectionBody>
+      <button onClick={handleLoadmoreProduct}>Load more</button>
+      {/* <Empty /> */}
     </Helmet>
   );
 }
--- project/Project-Nodejs-2/web-ban-hang/src/pages/Contact/index.js
+++ project/Project-Nodejs-2/web-ban-hang/src/pages/Contact/index.js
@@ -1,11 +1,65 @@
-import React from 'react';
- 
-import Helmet from '../../Components/Helmet';
+import React from "react";
 
-function Contact(props) {
-    return <Helmet title="Contact">Contact</Helmet>;
+import { css } from "styled-components";
+import Helmet from "../../Components/Helmet";
+
+export const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+  transition: display 0.5s ease;
+`;
+
+function Contact() {
+  //   const [loading, setLoading] = useState(false);
+
+  return (
+    <Helmet title="Liên Hệ">
+      <div className="contact">
+        <div className="contact_content">
+          <h2 className="contact_title_topic">LIÊN HỆ VỚI CHÚNG TÔI</h2>
+          <p className="contact_title_icon">
+            Tuấn store <i className="fas fa-store"></i>
+          </p>
+        </div>
+        <div className="contact_container">
+          <div className="contact_container_Infor">
+            <div className="contact_container_Infor_box">
+              <div className="contact_container_box_icon">
+                <i class="fas fa-map-marker-alt"></i>
+              </div>
+              <div className="contact_container_box_title">
+                <h2>Địa Chỉ</h2>
+                <p>125 Nguyễn Văn Linh, Quận Hải Châu, TP. Đà Nẵng</p>
+              </div>
+            </div>
+            <div className="contact_container_Infor_box">
+              <div className="contact_container_box_icon">
+                <i class="fas fa-phone-alt"></i>
+              </div>
+              <div className="contact_container_box_title">
+                <h2>Điện Thoại</h2>
+                <p>0935278930</p>
+              </div>
+            </div>
+            <div className="contact_container_Infor_box">
+              <div className="contact_container_box_icon">
+                <i class="fas fa-envelope"></i>
+              </div>
+              <div className="contact_container_box_title">
+                <h2>Email</h2>
+                <p>PhamTuan02051998@gmail.com</p>
+              </div>
+            </div>
+          </div>
+          {/* <div className="contact_container_Infor_customer">
+            <div className="contact_container_Infor_customer_titlefb"></div>
+            <div className="contact_container_Infor_customer_title">edwww</div>
+          </div> */}
+        </div>
+      </div>
+    </Helmet>
+  );
 }
-
-Contact.propTypes = {};
 
 export default Contact;
